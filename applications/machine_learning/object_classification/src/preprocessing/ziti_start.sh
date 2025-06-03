f068ff39d759f81a2f15f59e7059ee14a9376490
hong3nguyen <tringuyennht@gmail.com>
2025-06-03 13:19:25 +0300

    add error from edditing hosts in preprocessing

--- applications/machine_learning/object_classification/src/preprocessing/ziti_start.sh
+++ applications/machine_learning/object_classification/src/preprocessing/ziti_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed 's/^::1/#&/' /etc/hosts >/tmp/hosts && cat /tmp/hosts >/etc/hosts
+
 # ziti-edge-tunnel takeing sceret from opt
 ziti-edge-tunnel run -i /opt/secret.json >/dev/null 2>&1 &
 
