48ffc8ae3d30570d6229197b43e2c824ed184003
hong3nguyen <tringuyennht@gmail.com>
2025-06-16 15:19:55 +0300

    edit ziti run

--- zerotrust_config/openziti/hostZT-setting/cloud-gcp/setup_cloud_ctrl_router.sh
+++ zerotrust_config/openziti/hostZT-setting/cloud-gcp/setup_cloud_ctrl_router.sh
@@ -76,38 +76,38 @@ systemctl enable --now ziti-router.service
 ####################################
 
 # ensemble to messageQ
-ziti edge create config "ensemble" intercept.v1 \
+${ZITI_HOME}/bin/ziti edge create config "ensemble" intercept.v1 \
   '{"protocols":["tcp"],"addresses":["rabbitmq.ziti-controller.private"], "portRanges":[{"low":5672, "high":5672}]}'
 
-ziti edge create config "message-queue" host.v1 \
+${ZITI_HOME}/bin/ziti edge create config "message-queue" host.v1 \
   '{"protocol":"tcp", "address":"rabbitmq.ziti-controller.private","port":5672}'
 
-ziti edge create service "message-queue-service" \
+${ZITI_HOME}/bin/ziti edge create service "message-queue-service" \
   --configs ensemble-intercept-message-queue-config,message-queue-host-config
 
-ziti edge create service-policy "message-queue-bind-policy" Bind \
+${ZITI_HOME}/bin/ziti edge create service-policy "message-queue-bind-policy" Bind \
   --service-roles '@message-queue-service' --identity-roles '#message-queue' \
   --identity-roles '#cloud-only'
 
-ziti edge create service-policy "ensemble-dial-policy" Dial \
+${ZITI_HOME}/bin/ziti edge create service-policy "ensemble-dial-policy" Dial \
   --service-roles '@ensemble-service' --identity-roles '#ensemble' \
   --identity-roles '#cloud-only'
 
 # messageQ to database
-ziti edge create config "message-queue" intercept.v1 \
+${ZITI_HOME}/bin/ziti edge create config "message-queue" intercept.v1 \
   '{"protocols":["tcp"],"addresses":["database.ziti-controller.private"], "portRanges":[{"low":27017, "high":27017}]}'
 
-ziti edge create config "database" host.v1 \
+${ZITI_HOME}/bin/ziti edge create config "database" host.v1 \
   '{"protocol":"tcp", "address":"database.ziti-controller.private","port":27017}'
 
-ziti edge create service "database-service" \
+${ZITI_HOME}/bin/ziti edge create service "database-service" \
   --configs message-queue-intercept-database-config,database-host-config
 
-ziti edge create service-policy "database-bind-policy" Bind \
+${ZITI_HOME}/bin/ziti edge create service-policy "database-bind-policy" Bind \
   --service-roles '@database-service' --identity-roles '#database' \
   --identity-roles '#cloud-only'
 
-ziti edge create service-policy "message-queue-dial-policy" Dial \
+${ZITI_HOME}/bin/ziti edge create service-policy "message-queue-dial-policy" Dial \
   --service-roles '@message-queue-service' --identity-roles '#message-queue' \
   --identity-roles '#cloud-only'
 
@@ -121,11 +121,11 @@ ziti edge create service-policy "message-queue-dial-policy" Dial \
 
 #ziti edge update edge-router routerCloud  -a 'cloud'
 # assigne atribute to the router
-ziti edge update edge-router router_cloud -a 'cloud'
+${ZITI_HOME}/bin/ziti edge update edge-router router_cloud -a 'cloud'
 
 # edge router policy
-ziti edge create edge-router-policy allow.cloud --edge-router-roles '#cloud' --identity-roles '#cloud'
+${ZITI_HOME}/bin/ziti edge create edge-router-policy allow.cloud --edge-router-roles '#cloud' --identity-roles '#cloud'
 
-ziti edge create edge-router-policy cloud-only-routing \
+${ZITI_HOME}/bin/ziti edge create edge-router-policy cloud-only-routing \
   --identity-roles "#cloud-only" \
   --edge-router-roles "#cloud"
