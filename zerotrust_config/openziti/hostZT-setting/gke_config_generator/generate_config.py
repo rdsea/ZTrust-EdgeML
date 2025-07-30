36cbfafae12b78ba0d2263f2b410f1ef04c05cbc
hong3nguyen <tringuyennht@gmail.com>
2025-07-30 11:03:36 +0300

    add k3s and change edge to k3s folder

--- zerotrust_config/openziti/hostZT-setting/gke_config_generator/generate_config.py
+++ zerotrust_config/openziti/hostZT-setting/gke_config_generator/generate_config.py
@@ -70,10 +70,13 @@ if __name__ == "__main__":
 
     # --- Files to generate and their new output paths ---
     files_to_generate = {
+        "common.sh.tmpl": "scripts/common.sh",
         "gke_main_tf.tmpl": "gke/main.tf",
         "gke_script_setup_cluster.sh.tmpl": "scripts/gke_script_setup_cluster.sh",
         "gke_dns_configmap.yml.tmpl": "scripts/gke_dns_configmap.yml",
-        "k3s_ansible_inventory.yml.tmpl": "edge/k3s_ansible_inventory_test.local.yml",
+        "k3s_ansible_inventory.yml.tmpl": "k3s/k3s_ansible_inventory_test.local.yml",
+        "k3s_edge_cluster.sh.tmpl": "k3s/k3s_edge_cluster.sh",
+        "k3s_edge_setup_router.sh.tmpl": "k3s/k3s_edge_setup_router.sh",
     }
 
     ctrl_advertise_name = (
